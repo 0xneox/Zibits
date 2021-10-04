ce2fa8ca7f7b7e0b9958ae8f3a6d05f5efe3fd14
neozen <neohex262@gmail.com>
2021-10-05 02:41:05 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -28,7 +28,7 @@ export default function Home() {
           <div className="flex items-center justify-between w-full border-b-2	pb-6">
             <a href="/" className=""><img src="images/294.png" width="108" alt="" className="logo-image" /></a>
             <nav className="flex flex-wrap flex-row justify-around Poppitandfinchsans">
-              <a href="#about" className="text-2xl montserrat text-white hover:text-blau m-6">About</a>
+              <a href="#about" className="text-2xl montserrat text-white hover:text-black m-6">About</a>
               <a href="/mint" className="text-2xl montserrat text-white hover:text-black m-6">Mint</a>
               <a href="#traits" className="text-2xl montserrat text-white hover:text-black m-6">Mayan Glyphs</a>
               <a href="#contact" className="text-2xl montserrat text-white hover:text-black m-6">Contact</a>
@@ -53,11 +53,10 @@ export default function Home() {
                     <span className="text-white text-2xl montserrat"><strong>TOTAL SUPPLY: 10000</strong> MayanPunks.<br/><strong>PRICE: 0.05 ETH </strong>each.</span></p>
                     <iframe src="https://free.timeanddate.com/countdown/i7vcex5b/n107/cf11/cm0/cu4/ct0/cs1/ca2/co0/cr0/ss0/cacfff/cpcfff/pct/tcfff/fn3/fs100/szw448/szh189/iso2021-06-29T21:00:00" allowtransparency="true" frameBorder="0" width="425" height="25"></iframe>
                   </div>
-                  <img className="lg:w-1/2 w-3/4" img src="images/192.png" alt="" width="300px" className="feature-image"/>
+                  <img className="lg:w-1/2 w-3/4" img src="images/419.svg" alt="" width="350px" className="feature-image"/>
             </div>
             <div className="flex flex-col items-center">
-
-            <a href="/mint" className="mt-4 montserrat text-4xl border-6 bg-green  text-red hover:text-white p-2 ">Mint Now</a>
+            <a href="/mint" className="mt-4 montserrat text-5xl border-6 bg-green  text-red hover:text-white p-2 ">Mint Now</a>
                 
                 
             </div> 
@@ -82,56 +81,65 @@ export default function Home() {
                   </div>
                 </div>
                 <div id="gallery" className="   mx-6 py-6">
-                    <h2 className="text-blau Poppitandfinchsans text-7xl text-center">SIGNATURE SERIES GALLERY</h2>
+                    <h2 className="text-blau Montserrat-Light text-7xl text-center">Signature Series </h2>
                   <div className="flex flex-wrap  items-center mx-6   py-6 ">
                     <div className="md:w-1/2">
-                      <div className=" border-blue-300 p-2"><img src="images/421.jpg" alt="" width="600px" className="feature-image"/></div>
+                      <div className=" border-blue-300 p-2"><img src="images/518.svg" alt="" width="600px" className="feature-image"/></div>
 
                     </div>
                     <div className="md:w-1/2 flex flex-wrap">
-                      <div className=" p-2 w-1/2"><img src="images/421.jpg" alt="" width="400px" className="feature-image"/></div>
-                      <div className=" p-2 w-1/2"><img src="images/421.jpg" alt="" width="400px" className="feature-image"/></div>
-                      <div className=" p-2 w-1/2"><img src="images/421.jpg" alt="" width="400px" className="feature-image"/></div>
-                      <div className=" p-2 w-1/2"><img src="images/421.jpg" alt="" width="400px" className="feature-image"/></div>
+                      <div className=" p-2 w-1/2"><img src="images/502.svg" alt="" width="400px" className="feature-image"/></div>
+                      <div className=" p-2 w-1/2"><img src="images/477.svg" alt="" width="400px" className="feature-image"/></div>
+                      <div className=" p-2 w-1/2"><img src="images/789.svg" alt="" width="400px" className="feature-image"/></div>
+                      <div className=" p-2 w-1/2"><img src="images/481.svg" alt="" width="400px" className="feature-image"/></div>
 
                     </div>
                   </div>
                 </div>
-                <div id="roadmap" className=""  >
-              <h2 className="text-7xl text-center montserrat text-blau my-4">Mission</h2>
-              <p className="text-xl text-white text-center my-6  montserrat"> Simple. To be the largest contributor to charity in the NFT space, as measured by % <span className="font-bold"> of primary sales. </span>  
-              </p>
-              <p className="text-xl text-white my-6  montserrat">Ever since the inception of the <span className="text-blau text-5xl Poppitandfinchsans">MayanPunk</span> after <a target="_blank" href="https://twitter.com/thedigitalvee/status/1405896585142280192" className="underline text-black font-bold">this tweet</a>, our goal has been to <span className="text-blau text-5xl Poppitandfinchsans">GIVE BACK</span> as much as possible.
-              </p>
-              <p className="text-xl text-white my-6  montserrat"> A minimum of 
-              <span className="font-bold"> 25% of all sales</span> are donated to charity. 
-              </p>
-              <p className="text-xl text-white my-6  montserrat"> At 100% sellout, we pledge to donate <span className="font-bold"> 60 ETH </span> from primary sales.
-              </p>      
-              <h2 className="text-6xl text-center montserrat text-blau my-4">Roadmap</h2>
-              <ul className="">
-                <li className="text-xl text-white my-6  montserrat"><span className="font-bold"> 4 x 10 ETH donations: </span> The first of these will be made to <a target="_blank" href="https://girlswhocode.com/" className="underline text-black font-bold">Girls Who Code</a>, working to empower young women and <span className="font-bold"> close the gender gap </span> in technology. We know women are unrepresented in the NFT space - lets help to fix this! The remaining donations will be made via community vote using <a target="_blank" href="https://thegivingblock.com/" className="underline text-black font-bold">the Giving Block</a>.</li>
-                <li className="text-xl text-white my-6  montserrat"><span className="font-bold"> 20 ETH to Community Wallet: </span> Directed towards supporting creators that want to make a positive contribution to the NFT space.</li>
-                <li className="text-xl text-white my-6  montserrat"><span className="font-bold"> Secondary sales: 5% royalty </span> (2.5% donation, 2.5% to the team). Secondary sales are directed towards charities voted on by the community monthly.</li>
-
-              </ul>
-              <p className="text-xl text-white my-6  montserrat"> In addition, we have been, and continue to make <span className="font-bold underline"> high quality content for the NFT community </span> through our <span className="font-bold underline"> PROJECT IN PROGRESS </span> series, to help budding creators to along their journey. 
-              </p>
-              <p className="text-xl text-white my-6  montserrat"> All fully annotated source code for image generation and the smart contract will be made available after launch as a community resource. 
-              </p>
-
-
-              </div>
-             
 
               
 
+                <div id="traits" className="flex flex-wrap justify-around items-center  mx-6 py-6">
+                  <div className="border-4 border-blue-300 p-4"><img src="images/429.svg" alt="" width="300px" className="feature-image"/></div>
+                  <div className="flex flex-col justify-between mx-6 sm:w-1/2 w-4/5 py-6 ">
+          
+                    <p className="text-xl text-black my-6  montserrat"> Universal Metaverse:
+                    </p>
+                     <p className="text-xl text-white my-6  montserrat"> A universal accept reservation system in the Metaverse in which sysndicate pass access as a security check-in for filtering junks. Zib1 syndicate pass holders can redeem all future drops without spending gas fee.
+</p>
+                    <p className="text-xl text-black my-6  montserrat"> Fractional Existence
+                    </p>
+                     <p className="text-xl text-white my-6  montserrat"> Zentians exhibit properties of Zentianbits which are fractionalized zentians with unique and distinctive traits and attributes which is randomly combined to generate Zentian.</p>
+                    <p className="text-xl text-black my-6  montserrat"> Integrated Composition
+                    </p>
+                    <p className="text-xl text-white my-6  montserrat"> Each Zentianz consists of multiple aspects in various combinations. Within the same experience, one can see,multiple iterations. Also they are irreducible to non-interdependent components.
+                    </p>
+                    <p className="text-xl text-white my-6  montserrat"> Your support in this time of need is greatly appreciated!   
+                    </p>
+                  </div>
+                </div>
+                            
+
+                <div className="md:w-2/3 w-4/5 " id="about">
+       
+        
+       <div className="mt-6 border-b-2 py-6">
+         <div className="flex flex-wrap lg:flex-nowrap justify-around items-center">
+               <div className="lg:w-1/2 w-3/4">
+                 <h1 className="text-4xl montserrat text-white ">Cybernetic </h1>
+        
+                 <p className="text-2xl text-white my-6  montserrat">Here we <span className="text-5xl Poppitandfinch "></span> <span className="text-blau text-5xl Poppitandfinchsans">Mayans</span> <span className="text-5xl Poppitandfinch"></span>, specialize in the world's <span className="text-span-2 font-bold">finest Mayan Glyphs</span>.<br />‍<br />xxxx <br/>‍<span className="text-blau text-5xl Poppitandfinchsans">xx </span><span className="text-5xl Poppitandfinch ">and</span><span className="text-blau text-5xl Poppitandfinchsans"> xx</span> <br/>xx.
+                 </p>
+              </div>
+               <img className="lg:w-1/2 w-3/4" img src="images/181.svg" alt="" width="350px" className="feature-image"/>
+         </div>
+      
+         </div>
 
 
-
-              </div>   
+            </div>
               <div id="contact" className="flex flex-wrap justify-around items-center  mx-6 py-6">
-                  <div className=" p-4"><img src="images/267.png" alt="" width="150px" className="feature-image"/></div>
+                  <div className=" p-4"><img src="images/410.svg" alt="" width="150px" className="feature-image"/></div>
                   <div className="flex flex-col justify-between mx-6 sm:w-1/2 w-4/5 py-6 ">
                     <h2 className="text-blau montserrat text-6xl text-center">Contact</h2>
                     <p className="text-xl text-white my-6  montserrat"> We'd love to hear from you! 
@@ -141,10 +149,13 @@ export default function Home() {
                     Or alternatively, join our <a className="text-black underline font-bold" target="_blank" href="">Discord Server</a>.
                     </p>
                   </div>
+                  </div>
+              </div>
+            </div>
        
 
 
-              </div>   
-          </div>
+            
+   
     )
   }
