639067ddb992c337ec5170f51afbd552bd60fda8
neozen <neohex262@gmail.com>
2021-10-05 02:43:38 +0530

    Update mint.js

--- pages/mint.js
+++ pages/mint.js
@@ -109,41 +109,62 @@ export default function Mint() {
 
   return (
     <div id="bodyy" className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Mayan Glyphs</title>
-        <link rel="icon" href="/images/favicon.jpg" />
+       <Head>
+         <title>Mayan Glyphs</title>
+         <link rel="icon" href="/images/favicon.jpg" />
 
-        <meta property="og:title" content="Boring Bananas Co." key="ogtitle" />
-        <meta property="og:description" content="Random collection of Generative Mayan Glyphs stored onchain " key="ogdesc" />
-        <meta property="og:type" content="website" key="ogtype" />
-        <meta property="og:url" content="https://zib.one/" key="ogurl"/>
-        <meta property="og:image" content="https://boringbananas.co/images/Hola.gif" key="ogimage"/>
-        <meta property="og:site_name" content="https://zib.one/" key="ogsitename" />
+         <meta property="og:title" content="Boring Bananas Co." key="ogtitle" />
+         <meta property="og:description" content="Random collection of Generative Mayan Glyphs stored onchain " key="ogdesc" />
+         <meta property="og:type" content="website" key="ogtype" />
+         <meta property="og:url" content="https://zib.one/" key="ogurl"/>
+         <meta property="og:image" content="https://boringbananas.co/images/Hola.gif" key="ogimage"/>
+         <meta property="og:site_name" content="https://zib.one/" key="ogsitename" />
 
-        <meta name="twitter:card" content="summary_large_image" key="twcard"/>
-        <meta property="twitter:domain" content="Mayan Glyphs" key="twdomain" />
-        <meta property="twitter:url" content="https://zib.one/" key="twurl" />
-        <meta name="twitter:title" content="Mayan Glyphs" key="twtitle" />
-        <meta name="twitter:description" content="Random collection of Generative Mayan Glyphs stored onchain " key="twdesc" />
-        <meta name="twitter:image" content="https://zib.one/images/Hola.gif" key="twimage" />
-      </Head>
+         <meta name="twitter:card" content="summary_large_image" key="twcard"/>
+         <meta property="twitter:domain" content="Mayan Glyphs" key="twdomain" />
+         <meta property="twitter:url" content="https://zib.one/" key="twurl" />
+         <meta name="twitter:title" content="Mayan Glyphs" key="twtitle" />
+         <meta name="twitter:description" content="Random collection of Generative Mayan Glyphs stored onchain " key="twdesc" />
+         <meta name="twitter:image" content="https://zib.one/images/Hola.gif" key="twimage" />
+       </Head>
 
 
    <div >
-           <div className="flex items-center justify-between w-full border-b-2	pb-6">
-             <a href="/" className=""><img src="images/2.png" width="108" alt="" className="logo-image" /></a>
-             <nav className="flex flex-wrap flex-row justify-around Poppitandfinchsans">
-               <a href="#about" className="text-4xl montserrat text-white hover:text-black m-6">About</a>
-               <a href="/mint" className="text-4xl montserrat text-white hover:text-black m-6">Mint</a>
-               <a href="#traits" className="text-4xl montserrat text-white hover:text-black m-6">Mayan Glyphs</a>
-               <a href="#roadmap" className="text-4xl montserrat text-white hover:text-black m-6">Roadmap</a>
-               <a href="#team" className="text-4xl montserrat text-white hover:text-black m-6">Team</a>
-               <a href="#contact" className="text-4xl montserrat text-white hover:text-black m-6">Contact</a>
-             </nav>
+                  <div className="flex items-center justify-between w-full border-b-2	pb-6">
+                     <a href="/" className=""><img src="images/294.png" width="108" alt="" className="logo-image" /></a>
+                     <nav className="flex flex-wrap flex-row justify-around Poppitandfinchsans">
+                       <a href="#about" className="text-2xl montserrat text-white hover:text-black m-6">About</a>
+                       <a href="/mint" className="text-2xl montserrat text-white hover:text-black m-6">Mint</a>
+                       <a href="#traits" className="text-2xl montserrat text-white hover:text-black m-6">Mayan Glyphs</a>
+                       <a href="#contact" className="text-2xl montserrat text-white hover:text-black m-6">Contact</a>
+                     </nav>
 
-           </div>
+                   </div>
 
         </div>
+      <div className="md:w-2/3 w-4/5 " id="about">
+          <div className="mt-6 border-b-2 py-6">
+            <div className="flex flex-wrap lg:flex-nowrap justify-around items-center">
+                  <div className="lg:w-1/2 w-3/4">
+                    <h1 className="text-4xl montserrat text-white ">Cybernetic </h1>
+                    <h1 className="text-6xl montserrat text-blau ">Mayan Punks</h1>
+                    <p className="text-2xl text-white my-6  montserrat">Here we <span className="text-5xl Poppitandfinch "></span> <span className="text-blau text-5xl Poppitandfinchsans">Mayans</span> <span className="text-5xl Poppitandfinch"></span>, specialize in the world's <span className="text-span-2 font-bold">finest Mayan Glyphs</span>.<br />‍<br />xxxx <br/>‍<span className="text-blau text-5xl Poppitandfinchsans">xx </span><span className="text-5xl Poppitandfinch ">and</span><span className="text-blau text-5xl Poppitandfinchsans"> xx</span> <br/>xx.
+                    </p>
+                    <p className="text-2xl text-white my-6  montserrat">
+                    <strong>LAUNCH DATE:</strong> 29 September 2021. 3PM EST. <br />‍
+                    <span className="text-white text-2xl montserrat"><strong>TOTAL SUPPLY: 10000</strong> MayanPunks.<br/><strong>PRICE: 0.05 ETH </strong>each.</span></p>
+                    <iframe src="https://free.timeanddate.com/countdown/i7vcex5b/n107/cf11/cm0/cu4/ct0/cs1/ca2/co0/cr0/ss0/cacfff/cpcfff/pct/tcfff/fn3/fs100/szw448/szh189/iso2021-06-29T21:00:00" allowtransparency="true" frameBorder="0" width="425" height="25"></iframe>
+                  </div>
+                  <img className="lg:w-1/2 w-3/4" img src="images/419.svg" alt="" width="350px" className="feature-image"/>
+            </div>
+            <div className="flex flex-col items-center">
+            <a href="/mint" className="mt-4 montserrat text-5xl border-6 bg-green  text-red hover:text-white p-2 ">Mint Now</a>
+
+
+            </div>
+            </div>
+
+
 
         <div className="md:w-2/3 w-4/5">
        
@@ -180,6 +201,7 @@ export default function Mint() {
             </div>
  
           </div>  
-    </div>  
+    </div>
+    </div>
     )
   }
