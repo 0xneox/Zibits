782fae130bd497c9e7ea6bc2886f29a8fb94f2aa
neozen <neohex262@gmail.com>
2021-10-05 21:50:44 +0530

    Add files via upload

---
+++ pages/glyphs.js
@@ -0,0 +1,167 @@
+import Head from 'next/head'
+
+export default function Home() {
+
+  return (
+    <div id="bodyy" className="flex flex-col items-center justify-center min-h-screen py-2">
+    
+    
+    <Head>
+                <title>Mayan Punk</title>
+                <link rel="icon" href="/images/favicon-32x32.png" />
+                <meta property="og:title" content="MayanPunk" key="ogtitle" />
+                <meta property="og:description" content="Curated collection of Generative MayanPunk made from 550 Mayan hieroglyphs. 
+         " key="ogdesc" />
+                <meta property="og:type" content="website" key="ogtype" />
+                <meta property="og:url" content="https://mayanpunk.com/" key="ogurl"/>
+                <meta property="og:site_name" content="https://mayanpunk.com/" key="ogsitename" />
+                <meta name="twitter:card" content="summary_large_image" key="twcard"/>
+                <meta property="twitter:domain" content="MayanPunk" key="twdomain" />
+                <meta property="twitter:url" content="https://mayanpunk.com/" key="twurl" />
+                <meta name="twitter:title" content="MayanPunk" key="twtitle" />
+                <meta name="twitter:description" content="Curated collection of Generative MayanPunk made from 550 Mayan hieroglyphs. 
+         " key="twdesc" />
+        
+          </Head>
+
+          <div >
+            <div className="flex items-center justify-between w-full border-b-2	pb-6">
+              <a href="/" className=""><img src="images/502.svg" width="108" alt="" className="logo-image" /></a>
+              <nav className="flex flex-wrap flex-row justify-around Poppitandfinchsans">
+                <a href="#home" className="text-2xl Poppitandfinch text-white hover:text-black m-6">Home</a>
+                <a href="/about" className="text-2xl Poppitandfinchsans text-white hover:text-black m-6">About</a>
+                <a href="/mint" className="text-2xl Poppitandfinchsans text-white hover:text-black m-6">Mint</a>
+                <a href="/features" className="text-2xl Poppitandfinchsans text-white hover:text-black m-6">Features</a>
+                <a href="/mayans" className="text-2xl Poppitandfinchsans text-white hover:text-black m-6">Mayans</a>
+                <a href="/roadmap" className="text-2xl Poppitandfinchsans text-white hover:text-black m-6">Roadmap</a>
+                <a href="https://linktr.ee/mayanpunk" className="text-2xl Poppitandfinchsans text-white hover:text-black m-6">Communities</a>
+                <a href="/provision" className="text-2xl Poppitandfinchsans text-white hover:text-black m-6">Provision</a>
+                <a href="/contract" className="text-2xl Poppitandfinchsans text-white hover:text-black m-6">Contract</a>
+              </nav>
+            </div>
+          </div>
+        <div className="md:w-2/3 w-4/5 " id="about">
+       
+
+
+    
+
+                <div id="gallery" className="   mx-6 py-6">
+                <div id="traits" className="flex flex-wrap justify-around items-left mx-6 py-6">
+                <div className="flex flex-col justify-between mx-6 sm:w-1/2 w-/4 py-6 "><br></br>
+                  <h2 className="text-blau Poppitandfinch text-5xl ">Mayan Glyphs</h2>
+                 
+                 <div className="lg:w-/2 w-4/">
+                           
+                            <p className="text-xl text-white my-6  Poppitandfinch"> A universal accept reservation system in the Metaverse in which sysndicate pass access as a security check-in for filtering junks. Zib1 syndicate pass holders can redeem all future drops without spending gas fee.
+       </p>
+                           
+                         
+                         </div>
+                         </div>
+                       </div>
+                  <div className="flex flex-wrap  items-center mx-6   py-6 ">
+                  <div className="mt-6 border-b-2 py-6">
+            <div className="flex flex-wrap lg:flex-nowrap justify-around items-center">
+                  <div className="lg:w-1/2 w-3/4">
+                  
+                    <h1 className="text-4xl montserrat text-blau ">Hiero glyphs</h1>
+                    <p className="text-2xl text-white my-6  Poppitandfinch">
+An experiment involving an alchemy of Generative Art and Ancient Civilization.<br />‍<br />Curated collection of random generated MayanPunk mapped to the real world. 
+<br></br><br/>‍<span className="text-blau text-5xl Poppitandfinchsans">550 </span>
+<span className="text-blau text-5xl Poppitandfinchsans">10000 </span>   </p>
+                    <p className="text-2xl text-white my-6  Poppitandfinch">
+                     <br />‍
+                   </p>
+                   </div>
+                  <img className="lg:w-1/2 w-3/4" img src="images/419.svg" alt="" width="350px" className="feature-image"/>
+            </div>
+            </div>
+            <div className="mt-6 border-b-2 py-6">
+            <div className="flex flex-wrap lg:flex-nowrap justify-around items-center">
+                  <img className="lg:w-1/2 w-3/4" img src="images/419.svg" alt="" width="350px" className="feature-image"/>
+                  <div className="lg:w-1/2 w-3/4"> 
+                    <h1 className="text-4xl montserrat text-blau ">Emblem glyphs</h1>
+                    <p className="text-2xl text-white my-6  Poppitandfinch">An emblem glyph is a kind of royal title. It consists of a place name followed by a word which is the Classic Maya term for lord with a  well-attested etymology. Sometimes the title is introduced by an adjective,"holy, divine or sacred", resulting in the construction "holy [mayanpunk] lord". <br></br><br></br>Emblem glyph simply reflects the time when Mayanists could not read Classic Maya inscriptions and used a term to isolate specific recurring structural components of the written narratives</p>
+                    <p className="text-2xl text-white my-6  Poppitandfinch">
+                     <br />‍
+                   </p>
+                   </div>
+                 
+            </div>
+            </div>
+            <div className="mt-6 border-b-2 py-6">
+            <div className="flex flex-wrap lg:flex-nowrap justify-around items-center">
+                  <div className="lg:w-1/2 w-3/4">
+                  
+                    <h1 className="text-4xl montserrat text-blau ">Logosyllabic glyphs</h1>
+                    <p className="text-2xl text-white my-6  Poppitandfinch">
+An experiment involving an alchemy of Generative Art and Ancient Civilization.<br />‍<br />Curated collection of random generated MayanPunk mapped to the real world. 
+<br></br><br/>‍<span className="text-blau text-5xl Poppitandfinchsans">550 </span>
+<span className="text-blau text-5xl Poppitandfinchsans">10000 </span>   </p>
+                    <p className="text-2xl text-white my-6  Poppitandfinch">
+                     <br />‍
+                   </p>
+                   </div>
+                  <img className="lg:w-1/2 w-3/4" img src="images/419.svg" alt="" width="350px" className="feature-image"/>
+            </div>
+            </div>
+
+          <div id="traits" className="flex flex-wrap justify-around items-left mx-6 py-6">
+                 
+          <div className="lg:w-/2 w-4/">
+                    
+                     <p className="text-xl text-white my-6  Poppitandfinch"> A universal accept reservation system in the Metaverse in which sysndicate pass access as a security check-in for filtering junks. Zib1 syndicate pass holders can redeem all future drops without spending gas fee.
+</p>
+                    
+                  
+                  </div>
+                </div>
+                <div id="traits" className="flex flex-wrap justify-around  mx-6 py-6">
+   
+               
+                    <p className="text-blau Montserrat-Light text-2xl"> We are not pitching an NFT, but an understanding. 
+                    </p>
+                     <p className="text-xl text-white my-6  Poppitandfinch"> AA universal accept reservation system in the Metaverse in which sysndicate pass access as a security check-in for filtering junks. Zib1 syndicate pass holders can redeem all future drops without spending gas fee.
+                     A universal accept reservation system in the Metaverse in  universal accept reservation system in the Metaverse in which sysndicate pass access as a security check-in for filtering junks. Zib1 syndicate pass holders can redeem all future drops without spending gas fee.<span>
+<br></br>      AA universal accept reservation system in the Metaverse in which sysndicate pass access as a security check-in for filtering junks. Zib1 syndicate pass holders can redeem all future drops without spending gas fee.
+                     A universal accept reservation system in the Metaverse in  universal accept reservation system in the Metaverse in which sysndicate pass access as a security check-in for filtering junks. Zib1 syndicate pass holders can redeem all future drops without spending gas fee.
+</span></p>
+                    </div>
+                  
+                      
+          
+                  </div>
+                </div>
+
+              
+
+       
+        
+       
+        
+
+
+
+                <div className="flex items-center justify-between w-full border-b-2	pb-6">
+            <a href="/" className=""><img src="images/502.svg" width="108" alt="" className="logo-image" /></a>
+            <nav className="flex flex-wrap flex-row justify-around Poppitandfinchsans">
+              <a href="#home" className="text-2xl Poppitandfinchsans text-white hover:text-black m-6">Home</a>
+              <a href="/about" className="text-2xl Poppitandfinchsans text-white hover:text-black m-6">About</a>
+              <a href="/mint" className="text-2xl Poppitandfinchsans text-white hover:text-black m-6">Mint</a>
+              <a href="/features" className="text-2xl Poppitandfinchsans text-white hover:text-black m-6">FAQ</a>
+              <a href="/mayans" className="text-2xl Poppitandfinchsans text-white hover:text-black m-6">User Guide</a>
+              <a href="/roadmap" className="text-2xl Poppitandfinchsans text-white hover:text-black m-6">Mint Guide</a>
+              <a href="/linktree" className="text-2xl Poppitandfinchsans text-white hover:text-black m-6">Communities</a>
+            </nav>
+          </div>
+              </div>
+              
+            </div>
+       
+
+
+            
+   
+    )
+  }
