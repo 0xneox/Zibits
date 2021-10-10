cbf488a2aec23973e7fea655ae636f3e8cd1e75f
neozen <neohex262@gmail.com>
2021-10-10 23:21:27 +0530

    Update glyphs.js

--- pages/glyphs.js
+++ pages/glyphs.js
@@ -5,12 +5,13 @@ export default function Home() {
   return (
     <div id="bodyy" className="flex flex-col items-center justify-center min-h-screen py-2">
     
-    
-    <Head>
+  
+ 
+<Head>
                 <title>Mayan Punk</title>
                 <link rel="icon" href="/images/favicon-32x32.png" />
                 <meta property="og:title" content="MayanPunk" key="ogtitle" />
-                <meta property="og:description" content="Curated collection of Generative MayanPunk made from 550 Mayan hieroglyphs. 
+                <meta property="og:description" content="Curated collection of 10000 Generative MayanPunk made from 550 Mayan hieroglyphs. 
          " key="ogdesc" />
                 <meta property="og:type" content="website" key="ogtype" />
                 <meta property="og:url" content="https://mayanpunk.com/" key="ogurl"/>
@@ -19,24 +20,23 @@ export default function Home() {
                 <meta property="twitter:domain" content="MayanPunk" key="twdomain" />
                 <meta property="twitter:url" content="https://mayanpunk.com/" key="twurl" />
                 <meta name="twitter:title" content="MayanPunk" key="twtitle" />
-                <meta name="twitter:description" content="Curated collection of Generative MayanPunk made from 550 Mayan hieroglyphs. 
+                <meta name="twitter:description" content="Curated collection of 10000 Generative MayanPunk made from 550 Mayan hieroglyphs. 
          " key="twdesc" />
         
-          </Head>
+</Head>
 
           <div >
             <div className="flex items-center justify-between w-full border-b-2	pb-6">
-              <a href="/" className=""><img src="images/502.svg" width="108" alt="" className="logo-image" /></a>
+              <a href="/home" className=""><img src="images/502.svg" width="108" alt="" className="logo-image" /></a><span></span>
               <nav className="flex flex-wrap flex-row justify-around Poppitandfinchsans">
-                <a href="#home" className="text-2xl Poppitandfinch text-white hover:text-black m-6">Home</a>
+                <a href="/home" className="text-2xl Poppitandfinch text-white hover:text-black m-6">Home</a>
                 <a href="/about" className="text-2xl Poppitandfinchsans text-white hover:text-black m-6">About</a>
                 <a href="/mint" className="text-2xl Poppitandfinchsans text-white hover:text-black m-6">Mint</a>
-                <a href="/features" className="text-2xl Poppitandfinchsans text-white hover:text-black m-6">Features</a>
+                <a href="/glyphs" className="text-2xl Poppitandfinchsans text-white hover:text-black m-6">Glyphs</a>
                 <a href="/mayans" className="text-2xl Poppitandfinchsans text-white hover:text-black m-6">Mayans</a>
-                <a href="/roadmap" className="text-2xl Poppitandfinchsans text-white hover:text-black m-6">Roadmap</a>
+                <a href="/faq" className="text-2xl Poppitandfinchsans text-white hover:text-black m-6">FAQ</a>
                 <a href="https://linktr.ee/mayanpunk" className="text-2xl Poppitandfinchsans text-white hover:text-black m-6">Communities</a>
-                <a href="/provision" className="text-2xl Poppitandfinchsans text-white hover:text-black m-6">Provision</a>
-                <a href="/contract" className="text-2xl Poppitandfinchsans text-white hover:text-black m-6">Contract</a>
+              
               </nav>
             </div>
           </div>
@@ -74,12 +74,14 @@ An experiment involving an alchemy of Generative Art and Ancient Civilization.<b
                      <br />‍
                    </p>
                    </div>
-                  <img className="lg:w-1/2 w-3/4" img src="images/419.svg" alt="" width="350px" className="feature-image"/>
+                  <img className="lg:w-1/2 w-3/4" img src="images/h1.svg" alt="" width="230px" className="feature-image"/>
+                  <img className="lg:w-1/2 w-3/4" img src="images/h2.svg" alt="" width="230px" className="feature-image"/>
             </div>
             </div>
             <div className="mt-6 border-b-2 py-6">
             <div className="flex flex-wrap lg:flex-nowrap justify-around items-center">
-                  <img className="lg:w-1/2 w-3/4" img src="images/419.svg" alt="" width="350px" className="feature-image"/>
+                  <img className="lg:w-1/2 w-3/4" img src="images/181.svg" alt="" width="230px" className="feature-image"/>
+                  <img className="lg:w-1/2 w-3/4" img src="images/032.png" alt="" width="230px" className="feature-image"/>
                   <div className="lg:w-1/2 w-3/4"> 
                     <h1 className="text-4xl montserrat text-blau ">Emblem glyphs</h1>
                     <p className="text-2xl text-white my-6  Poppitandfinch">An emblem glyph is a kind of royal title. It consists of a place name followed by a word which is the Classic Maya term for lord with a  well-attested etymology. Sometimes the title is introduced by an adjective,"holy, divine or sacred", resulting in the construction "holy [mayanpunk] lord". <br></br><br></br>Emblem glyph simply reflects the time when Mayanists could not read Classic Maya inscriptions and used a term to isolate specific recurring structural components of the written narratives</p>
@@ -103,7 +105,8 @@ An experiment involving an alchemy of Generative Art and Ancient Civilization.<b
                      <br />‍
                    </p>
                    </div>
-                  <img className="lg:w-1/2 w-3/4" img src="images/419.svg" alt="" width="350px" className="feature-image"/>
+                  <img className="lg:w-1/2 w-3/4" img src="images/181.svg" alt="" width="230px" className="feature-image"/>
+                  <img className="lg:w-1/2 w-3/4" img src="images/181.svg" alt="" width="230px" className="feature-image"/>
             </div>
             </div>
 
@@ -122,6 +125,17 @@ An experiment involving an alchemy of Generative Art and Ancient Civilization.<b
                
                     <p className="text-blau Montserrat-Light text-2xl"> We are not pitching an NFT, but an understanding. 
                     </p>
+                    <div className="mt-6 border-b-2 py-6">
+            <div className="flex flex-wrap lg:flex-nowrap justify-around items-center">
+                  <img className="lg:w-1/2 w-3/4" img src="images/421.svg" alt="" width="230px" className="feature-image"/>
+                  <img className="lg:w-1/2 w-3/4" img src="images/421.svg" alt="" width="230px" className="feature-image"/>
+                 
+                  <img className="lg:w-1/2 w-3/4" img src="images/421.svg" alt="" width="230px" className="feature-image"/>
+                  <img className="lg:w-1/2 w-3/4" img src="images/421.svg" alt="" width="230px" className="feature-image"/>
+                  
+                 
+            </div>
+            </div>
                      <p className="text-xl text-white my-6  Poppitandfinch"> AA universal accept reservation system in the Metaverse in which sysndicate pass access as a security check-in for filtering junks. Zib1 syndicate pass holders can redeem all future drops without spending gas fee.
                      A universal accept reservation system in the Metaverse in  universal accept reservation system in the Metaverse in which sysndicate pass access as a security check-in for filtering junks. Zib1 syndicate pass holders can redeem all future drops without spending gas fee.<span>
 <br></br>      AA universal accept reservation system in the Metaverse in which sysndicate pass access as a security check-in for filtering junks. Zib1 syndicate pass holders can redeem all future drops without spending gas fee.
@@ -144,21 +158,24 @@ An experiment involving an alchemy of Generative Art and Ancient Civilization.<b
 
 
                 <div className="flex items-center justify-between w-full border-b-2	pb-6">
-            <a href="/" className=""><img src="images/502.svg" width="108" alt="" className="logo-image" /></a>
+            <a href="/home" className=""><img src="images/502.svg" width="108" alt="" className="logo-image" /></a>
             <nav className="flex flex-wrap flex-row justify-around Poppitandfinchsans">
-              <a href="#home" className="text-2xl Poppitandfinchsans text-white hover:text-black m-6">Home</a>
-              <a href="/about" className="text-2xl Poppitandfinchsans text-white hover:text-black m-6">About</a>
+              <a href="/home" className="text-2xl Poppitandfinchsans text-white hover:text-black m-6">Home</a>
+              <a href="/gallery" className="text-2xl Poppitandfinchsans text-white hover:text-black m-6">Collection</a>
               <a href="/mint" className="text-2xl Poppitandfinchsans text-white hover:text-black m-6">Mint</a>
-              <a href="/features" className="text-2xl Poppitandfinchsans text-white hover:text-black m-6">FAQ</a>
-              <a href="/mayans" className="text-2xl Poppitandfinchsans text-white hover:text-black m-6">User Guide</a>
-              <a href="/roadmap" className="text-2xl Poppitandfinchsans text-white hover:text-black m-6">Mint Guide</a>
-              <a href="/linktree" className="text-2xl Poppitandfinchsans text-white hover:text-black m-6">Communities</a>
+              <a href="/faq" className="text-2xl Poppitandfinchsans text-white hover:text-black m-6">FAQ</a>
+              <a href="/about" className="text-2xl Poppitandfinchsans text-white hover:text-black m-6">About US</a>
+              <a href="/roadmap" className="text-2xl Poppitandfinchsans text-white hover:text-black m-6">Roadmap</a>
+              <a href="https://rinkeby.etherscan.io/address/0x805b19163109aa9506383c28c191f55bb882262f" className="text-2xl Poppitandfinchsans text-white hover:text-black m-6">Testnet</a>
+
+              <a href="https://discord.gg/9pNpXBDwqD" className="text-2xl Poppitandfinchsans text-blau   m-6">Discord</a>
+              <a href="https://twitter.com/mayanpunk" className="text-2xl Poppitandfinchsans text-blau  m-6">Twitter</a>
+             
             </nav>
           </div>
-              </div>
-              
-            </div>
-       
+        </div>
+        <p className="text-1xl text-white my-6  Poppitandfinch">All rights granted (c) <span className="text-blau text-1xl Poppitandfinchsans">MayanPunk </span> Fork it  <a href="https://github.com/Mayanpunk" className="text-blau text-1xl Poppitandfinchsans">Github </a></p>
+     </div>
 
 
             
