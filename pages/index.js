c11f53febb067ba0028fa5093358ded2a8eaef4b
neozen <neohex262@gmail.com>
2021-10-10 23:22:01 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
                 <title>Mayan Punk</title>
                 <link rel="icon" href="/images/favicon-32x32.png" />
                 <meta property="og:title" content="MayanPunk" key="ogtitle" />
-                <meta property="og:description" content="Curated collection of Generative MayanPunk made from 550 Mayan hieroglyphs. 
+                <meta property="og:description" content="Curated collection of 10000 Generative MayanPunk made from 550 Mayan hieroglyphs. 
          " key="ogdesc" />
                 <meta property="og:type" content="website" key="ogtype" />
                 <meta property="og:url" content="https://mayanpunk.com/" key="ogurl"/>
@@ -18,21 +18,21 @@ export default function Home() {
                 <meta property="twitter:domain" content="MayanPunk" key="twdomain" />
                 <meta property="twitter:url" content="https://mayanpunk.com/" key="twurl" />
                 <meta name="twitter:title" content="MayanPunk" key="twtitle" />
-                <meta name="twitter:description" content="Curated collection of Generative MayanPunk made from 550 Mayan hieroglyphs. 
+                <meta name="twitter:description" content="Curated collection of 10000 Generative MayanPunk made from 550 Mayan hieroglyphs. 
          " key="twdesc" />
         
 </Head>
 
           <div >
             <div className="flex items-center justify-between w-full border-b-2	pb-6">
-              <a href="/" className=""><img src="images/502.svg" width="108" alt="" className="logo-image" /></a><span></span>
+              <a href="/home" className=""><img src="images/502.svg" width="108" alt="" className="logo-image" /></a><span></span>
               <nav className="flex flex-wrap flex-row justify-around Poppitandfinchsans">
-                <a href="#home" className="text-2xl Poppitandfinch text-white hover:text-black m-6">Home</a>
+                <a href="/home" className="text-2xl Poppitandfinch text-white hover:text-black m-6">Home</a>
                 <a href="/about" className="text-2xl Poppitandfinchsans text-white hover:text-black m-6">About</a>
                 <a href="/mint" className="text-2xl Poppitandfinchsans text-white hover:text-black m-6">Mint</a>
                 <a href="/glyphs" className="text-2xl Poppitandfinchsans text-white hover:text-black m-6">Glyphs</a>
                 <a href="/mayans" className="text-2xl Poppitandfinchsans text-white hover:text-black m-6">Mayans</a>
-                <a href="/roadmap" className="text-2xl Poppitandfinchsans text-white hover:text-black m-6">Docs</a>
+                <a href="/faq" className="text-2xl Poppitandfinchsans text-white hover:text-black m-6">FAQ</a>
                 <a href="https://linktr.ee/mayanpunk" className="text-2xl Poppitandfinchsans text-white hover:text-black m-6">Communities</a>
               
               </nav>
@@ -48,7 +48,7 @@ export default function Home() {
 An experiment involving an alchemy of Generative Art and Ancient Civilization.<br />‍<br />Curated collection of random generated MayanPunk mapped to the real world. <br></br><br/>‍<span className="text-blau text-5xl Poppitandfinchsans">550 </span><span className="text-5xl Poppitandfinch "> Mayan</span><span className="text-blau text-5xl Poppitandfinchsans"> Hieroglyphs </span> 
 <span className="text-blau text-5xl Poppitandfinchsans">7777 </span><span className="text-5xl Poppitandfinch "> Mayan</span><span className="text-blau text-5xl Poppitandfinchsans"> Punk </span>     </p>
                                 </div>
-                  <img className="lg:w-1/2 w-3/4" img src="images/419.svg" alt="" width="350px" className="feature-image"/>
+                  <img className="lg:w-1/2 w-3/4" img src="images/15.svg" alt="" width="350px" className="feature-image"/>
             </div>
             </div>
                 <div id="traits" className="flex flex-wrap justify-around items-center  mx-6 py-6">
@@ -72,7 +72,7 @@ The Maya script was a logosyllabic system with some syllabogrammatic elements. I
                   </div>
                 </div>
                 <div id="traits" className="flex flex-wrap justify-around items-center  mx-6 py-6">
-                  <div className="border-4 border-blue-300 p-4"><img src="images/429.svg" alt="" width="300px" className="feature-image"/></div>
+                  <div className="border-4 border-blue-300 p-4"><img src="images/a1.svg" alt="" width="300px" className="feature-image"/></div>
                   <div className="flex flex-col justify-between mx-6 sm:w-1/2 w-4/5 py-6 ">
           <p className="text-blau Montserrat-Light text-3xl"> Hiero glyphs
                     </p>
@@ -99,19 +99,19 @@ The Maya script was a logosyllabic system with some syllabogrammatic elements. I
                  
                 </div>
                 <div id="traits" className="flex flex-wrap justify-around items-center  mx-6 py-6">
-                <div className="border-4 border-blue-300 p-4"><img src="images/1.svg" alt="" width="100px" className="feature-image"/></div>
-                <div className="border-4 border-blue-300 p-4"><img src="images/2.svg" alt="" width="100px" className="feature-image"/></div>
-                <div className="border-4 border-blue-300 p-4"><img src="images/3.svg" alt="" width="100px" className="feature-image"/></div>
-                <div className="border-4 border-blue-300 p-4"><img src="images/4.svg" alt="" width="100px" className="feature-image"/></div>
+                <div className="border-4 border-blue-300 p-4"><img src="images/181.svg" alt="" width="100px" className="feature-image"/></div>
+                <div className="border-4 border-blue-300 p-4"><img src="images/294.png" alt="" width="100px" className="feature-image"/></div>
+                <div className="border-4 border-blue-300 p-4"><img src="images/051.svg" alt="" width="110px" className="feature-image"/></div>
+                <div className="border-4 border-blue-300 p-4"><img src="images/4.svg" alt="" width="110px" className="feature-image"/></div>
                 <div className="border-4 border-blue-300 p-4"><img src="images/5.svg" alt="" width="100px" className="feature-image"/></div>
-                <div className="border-4 border-blue-300 p-4"><img src="images/6.svg" alt="" width="100px" className="feature-image"/></div>
-                </div><div id="traits" className="flex flex-wrap justify-around items-center  mx-6 py-6">
-                <div className="border-4 border-blue-300 p-4"><img src="images/7.svg" alt="" width="100px" className="feature-image"/></div>
                 <div className="border-4 border-blue-300 p-4"><img src="images/8.svg" alt="" width="100px" className="feature-image"/></div>
-                <div className="border-4 border-blue-300 p-4"><img src="images/9.svg" alt="" width="100px" className="feature-image"/></div>
-                <div className="border-4 border-blue-300 p-4"><img src="images/10.svg" alt="" width="100px" className="feature-image"/></div>
-                <div className="border-4 border-blue-300 p-4"><img src="images/11.svg" alt="" width="100px" className="feature-image"/></div>
-                <div className="border-4 border-blue-300 p-4"><img src="images/6.svg" alt="" width="100px" className="feature-image"/></div>
+                </div><div id="traits" className="flex flex-wrap justify-around items-center  mx-6 py-6">
+                <div className="border-4 border-blue-300 p-4"><img src="images/92.png" alt="" width="110px" className="feature-image"/></div>
+                <div className="border-4 border-blue-300 p-4"><img src="images/maya006.svg" alt="" width="110px" className="feature-image"/></div>
+                <div className="border-4 border-blue-300 p-4"><img src="images/xc1.svg" alt="" width="110px" className="feature-image"/></div>
+                <div className="border-4 border-blue-300 p-4"><img src="images/xc2.png" alt="" width="100px" className="feature-image"/></div>
+                <div className="border-4 border-blue-300 p-4"><img src="images/432.svg" alt="" width="110px" className="feature-image"/></div>
+                <div className="border-4 border-blue-300 p-4"><img src="images/asw.png" alt="" width="110px"  className="feature-image"/></div>
                 </div>
 
                 <div id="traits" className="flex flex-wrap justify-around  mx-6 py-6">
@@ -130,9 +130,9 @@ The Maya script was a logosyllabic system with some syllabogrammatic elements. I
                 <div id="traits" className="flex flex-wrap justify-around  mx-6 py-6">
         
                     <br></br>
-                    <p className="text-xl text-white my-6  Montserrat-Light"> Drop us a line at <a className="text-blau underline font-bold" target="_blank" href="mailto:mayanpunk@pm.me">mayanpunk@pm.me</a>,<br/>
+                    <p className="text-xl text-white my-6  Montserrat-Light"> Drop us a line at <a className="text-blau underline font-bold" target="_blank" href="mailto:mayanpunk@pm.me">neohex@pm.me</a>,<br/>
                     or alternatively reach out to us on Twitter <a className="text-black underline font-bold" target="_blank" href="https://twitter.com/mayanpunk">@Mayanpunk</a><br/>
-                    Or alternatively, join our <a className="text-black underline font-bold" target="_blank" href="">Discord Server</a>.
+                    Or alternatively, join our <a className="text-black underline font-bold" target="https://discord.gg/9pNpXBDwqD" href="https://discord.gg/9pNpXBDwqD">Discord Server</a>.
                     </p>
                  </div>
      
@@ -140,18 +140,18 @@ The Maya script was a logosyllabic system with some syllabogrammatic elements. I
 
            <br></br>
           <div className="flex items-center justify-between w-full border-b-2	pb-6">
-            <a href="/" className=""><img src="images/502.svg" width="108" alt="" className="logo-image" /></a>
+            <a href="/home" className=""><img src="images/502.svg" width="108" alt="" className="logo-image" /></a>
             <nav className="flex flex-wrap flex-row justify-around Poppitandfinchsans">
-              <a href="#Shome" className="text-2xl Poppitandfinchsans text-white hover:text-black m-6">Home</a>
-              <a href="/about" className="text-2xl Poppitandfinchsans text-white hover:text-black m-6">Collection</a>
+              <a href="/home" className="text-2xl Poppitandfinchsans text-white hover:text-black m-6">Home</a>
+              <a href="/gallery" className="text-2xl Poppitandfinchsans text-white hover:text-black m-6">Collection</a>
               <a href="/mint" className="text-2xl Poppitandfinchsans text-white hover:text-black m-6">Mint</a>
               <a href="/faq" className="text-2xl Poppitandfinchsans text-white hover:text-black m-6">FAQ</a>
-              <a href="/userguide" className="text-2xl Poppitandfinchsans text-white hover:text-black m-6">User Guide</a>
-              <a href="/mintguide" className="text-2xl Poppitandfinchsans text-white hover:text-black m-6">Mint Guide</a>
-              <a href="/mintguide" className="text-2xl Poppitandfinchsans text-white hover:text-black m-6">Onchain</a>
+              <a href="/about" className="text-2xl Poppitandfinchsans text-white hover:text-black m-6">About US</a>
+              <a href="/roadmap" className="text-2xl Poppitandfinchsans text-white hover:text-black m-6">Roadmap</a>
+              <a href="https://rinkeby.etherscan.io/address/0x805b19163109aa9506383c28c191f55bb882262f" className="text-2xl Poppitandfinchsans text-white hover:text-black m-6">Testnet</a>
 
-              <a href="https://github.com/Mayanpunk" className="text-2xl Poppitandfinchsans text-blau   m-6">Discord</a>
-              <a href="https://github.com/Mayanpunk" className="text-2xl Poppitandfinchsans text-blau  m-6">Twitter</a>
+              <a href="https://discord.gg/9pNpXBDwqD" className="text-2xl Poppitandfinchsans text-blau   m-6">Discord</a>
+              <a href="https://twitter.com/mayanpunk" className="text-2xl Poppitandfinchsans text-blau  m-6">Twitter</a>
              
             </nav>
           </div>
